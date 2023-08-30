2a16b460afbc99edde9bc9f33e89571b60e93452
zikra-abdusemed <zikraabdusemed1@gmail.com>
2023-08-29 23:31:55 -0700

    first commit

---
+++ FrontEnd/script.js
@@ -0,0 +1,6 @@
+const body = document.body
+const div = document.querySelector('div')
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.removeAtr
